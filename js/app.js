1dca189a599e29cbbf5dd3382cdb97fcd1f08feb
ericstretton <eric.stretton@gmail.com>
2022-03-18 19:42:21 -0600

    Started Assignment W9C.

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var tweets = [ "Sick of having to go to 2 different huts to buy pizza & sunglasses.", 
+                "Waabalubbadubbdub", "This is the most annoying part of the assignment",
+                "I hate applying concepts from my English high school experiences",
+                "Meh, directing my dislike for creating content towards creating content seems to be working",
+                "I hope I am doing this first step correctly...",
+                "MMMMM pretty sure six isn't enough tweets yet, well here's seven then!",
+                "Thankful for machines and how I hopefully won't have to re-type this nonsense a million times!!!"
+                "You know, I'm sure I could have just done shorter quotes...",
+                "True!"]
+console.log(tweets);
